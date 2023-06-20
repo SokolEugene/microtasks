f369e60155ddc64e6ef41a06352d4c97ac5a809e
Eugene Skl <zheksem@gmail.com>
2023-06-20 15:54:51 +0300

    components and props

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {Header} from './site/Header';
+import {Body} from './site/Body';
+import {Footer} from './site/Footer';
+
+function App() {
+    return (
+        <div className="App">
+            <Header titleForHeader={'NEW Header'}/>
+            <Body titleForBody={'New Body'}/>
+            <Footer titleForFooter={'Ney Footer'}/>
+            {/* <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/site/Header.tsx
@@ -0,0 +1,12 @@
+
+type HeaderType ={
+    titleForHeader:string
+}
+
+export const Header = (props:HeaderType) => {
+    return(
+        <>
+            {props.titleForHeader}
+        </>
+    )
+}
