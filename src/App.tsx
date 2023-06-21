78ffa167dde502366efbe357aab25f41a4fc84d7
Eugene Skl <zheksem@gmail.com>
2023-06-21 15:23:28 +0300

    buttons and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,40 @@
 import './App.css';
-import {Header} from './site/Header';
-import {Body} from './site/Body';
-import {Footer} from './site/Footer';
-import {NewComponent} from './site/NewComponent';
-import {TableComponent} from './site/TableComponent';
+import React, {useState} from 'react';
+
+import {Button} from './Components/Button';
 
 function App() {
-    const topCars = [
-        {manufacturer: 'BMW', model: 'm5cs'},
-        {manufacturer: 'Mercedes', model: 'e63s'},
-        {manufacturer: 'Audi', model: 'rs6'}
-    ]
-    const students = [
-        {id: 1, name: 'James', age: 8},
-        {id: 2, name: 'Robert', age: 18},
-        {id: 3, name: 'John', age: 28},
-        {id: 4, name: 'Michael', age: 38},
-        {id: 5, name: 'William', age: 48},
-        {id: 6, name: 'David', age: 58},
-        {id: 7, name: 'Richard', age: 68},
-        {id: 8, name: 'Joseph', age: 78},
-        {id: 9, name: 'Thomas', age: 88},
-        {id: 10, name: 'Charles', age: 98},
-        {id: 11, name: 'Christopher', age: 100},
-        {id: 12, name: 'James', age: 8},
-        {id: 13, name: 'Robert', age: 18},
-        {id: 14, name: 'John', age: 28},
-        {id: 15, name: 'Michael', age: 38},
-        {id: 16, name: 'William', age: 48},
-        {id: 17, name: 'David', age: 58},
-        {id: 18, name: 'Richard', age: 68},
-        {id: 19, name: 'Joseph', age: 78},
-        {id: 20, name: 'Thomas', age: 88},
-        {id: 21, name: 'Charles', age: 98},
-        {id: 22, name: 'Christopher', age: 100},
-    ]
-
-
-
-    const onClickHandler=(name:string)=>{
-        console.log(`Hello ${name}`)
+    const Button1Foo = (subscriber: string, age: number, adres: string) => {
+        console.log(`My name is ${subscriber} and I am ${age} and also i live in ${adres}`)
     }
+    const Button2Foo = (subscriber: string, age: number, adres: string) => {
+        console.log(`My name is ${subscriber} and I am ${age} and also i live in ${adres}`)
+    }
+    const Button3Foo = () => {
+        console.log('Be patient, I have autism')
 
 
+        let [a, setA] = useState(0)
+        const onClickHandler = () => {
+            setA(++a);
+            console.log(a)
+        }
 
-    return (
-        <div className="App">
-            <Header titleForHeader={'NEW Header'}/>
-            <Body titleForBody={'New Body'}/>
-            <Footer titleForFooter={'Ney Footer'}/>
-            <NewComponent students={students}/>
-            <TableComponent topCars={topCars}/>
-            <button onClick={()=>onClickHandler('Vasya')}>MyYTChanel -1</button>
-            <button onClick={()=>onClickHandler('Ivan')}>MyYTChanel -2</button>
+        const onClickHandler1 = () => {
+            setA(0);
+            console.log(a)
 
-
-            { /* <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>*/}
-        </div>
-    );
+        }
+        return (
+            <div className="App">
+                <Button name={'MyYTChanel -1'} callBack={() => Button1Foo('Vasya', 21, 'Minsk')}/>
+                <Button name={'MyYTChanel -2'} callBack={() => Button2Foo('Ivan', 22, 'Vitebsk')}/>
+                <Button name={'Stupid Button'} callBack={Button3Foo}/>
+                <h1>{a}</h1>
+                <button onClick={onClickHandler}>number</button>
+                <button onClick={onClickHandler1}>0</button>
+            </div>
+        )
+    }
 }
-
 export default App;
